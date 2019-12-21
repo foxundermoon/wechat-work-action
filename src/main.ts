c705866adaecd4e01b3ca29b797a24a183db6208
fox <fox@vvfox.com>
2019-12-21 22:43:40 +0800

    complete

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,47 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import got from 'got'
+
+enum MsgType {
+  text = 'text',
+  markdown = 'markdown',
+  custom = 'custom'
+}
+async function postMessage(): Promise<string> {
+  const type: MsgType = core.getInput('type') as MsgType
+  const content: string = core.getInput('content')
+  const at: string = core.getInput('at')
+
+  if (type === MsgType.custom) {
+    const payload = JSON.parse(content)
+    return post(payload)
+  }
+
+  const payload = {
+    msgtype: type,
+    [type]: {
+      content: content
+    },
+    mentioned_list: at.toLowerCase().includes('all') ? ['@all'] : at.split(',')
+  }
+  return post(payload)
+}
+
+async function post(body: any): Promise<string> {
+  const url: string = core.getInput('url')
+  const rsp = await got.post(url, {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+
+  console.log(rsp.body)
+  return rsp.body
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    await postMessage()
   } catch (error) {
     core.setFailed(error.message)
   }
